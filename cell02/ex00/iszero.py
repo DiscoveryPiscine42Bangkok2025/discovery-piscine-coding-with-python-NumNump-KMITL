d9997c6e67b5037b620321ce7f05da17b61952e0
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-15 22:39:25 +0700

    ex2.0 : Am I Null

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,10 @@
+num_input = int(input("Enter a number: "))
+
+if num_input == 42 :
+    number =  int(input(""))
+    if number == 0 :
+        print("This number is equal to zero")
+    else :
+        print("This number is different from zero")
+else :
+    exit(1)
