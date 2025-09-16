ad97f607882986b6c27b24694c615827f7cc72a0
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-16 20:13:18 +0700

    Play with Arrays

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,9 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_arrray = []
+
+for i in original_array : 
+    new_arrray.append(i+2)
+
+print("Origin array : ",original_array)
+print("New array : ",new_arrray)
