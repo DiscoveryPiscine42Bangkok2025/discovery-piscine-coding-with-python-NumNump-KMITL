72713847cf5c3a788a08d17032f5a320039c158b
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-17 00:40:47 +0700

    fix ex01

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -2,8 +2,8 @@ age_input = input("Please tell me your age : ")
 
 age = int(age_input)
 
-print(f"You are currently {15} years old.")
+print(f"You are currently {age} years old.")
 
-print(f"In 10 years, you'll be {15+10} years old.")
-print(f"In 20 years, you'll be {15+20} years old.")
-print(f"In 30 years, you'll be {15+30} years old.")
+print(f"In 10 years, you'll be {age+10} years old.")
+print(f"In 20 years, you'll be {age+20} years old.")
+print(f"In 30 years, you'll be {age+30} years old.")
