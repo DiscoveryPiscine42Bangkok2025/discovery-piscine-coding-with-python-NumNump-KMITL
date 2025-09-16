ba7c244eec465db13e30c29320b24b844009bff7
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-16 20:17:30 +0700

    ex5.2 : Play with Arrays++

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,10 @@
+original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+
+new_arrray = []
+
+for i in original_array : 
+    if i > 5 :
+        new_arrray.append(i+2)
+
+print("Origin array : ",original_array)
+print("New array : ",new_arrray)
