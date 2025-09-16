b48f4299aadb9eac81eeb8809e14bf03a2065b12
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-16 10:32:48 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,7 +1,6 @@
 while True:
     user_input = input("What you gotta say? : ").strip()
     if user_input == "STOP":
-        # เมื่อพิมพ์ STOP (ไม่สนตัวใหญ่/เล็ก) จะจบโปรแกรม
         break
     else:
         print("I got that! Anything else? : ", end="")
