ace6915ace5af3177c1760ef823ea48fb49a9a51
NumNump-KMITL <65050741@kmitl.ac.th>
2025-09-16 15:19:06 +0700

    ex4.1 : Age

---
+++ cell04/ex01/age.py
@@ -0,0 +1,9 @@
+age_input = input("Please tell me your age : ")
+
+age = int(age_input)
+
+print(f"You are currently {15} years old.")
+
+print(f"In 10 years, you'll be {15+10} years old.")
+print(f"In 20 years, you'll be {15+20} years old.")
+print(f"In 30 years, you'll be {15+30} years old.")
